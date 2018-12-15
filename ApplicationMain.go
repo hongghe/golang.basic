0066cd946cc88814ff290c78bdd6a5dd200314d0
hongghe <364930855@qq.com>
2018-12-15 11:06:19 +0800

    The http default request

--- ApplicationMain.go
+++ ApplicationMain.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
+// the handler of http request and response
+func handler(writer http.ResponseWriter, request *http.Request)  {
+	fmt.Fprint(writer, "Hello world!", request.URL.Path[1:])
+}
+
+
+// the main function of project.
 func main() {
-	print("The main function.")
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
